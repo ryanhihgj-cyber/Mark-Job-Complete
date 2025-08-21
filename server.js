487a1f0eb6d439ea33e62cc11418928cb5cb48a0
ryanhihgj-cyber <ryanhihgj@gmail.com>
2025-08-21 14:45:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post('/mark-complete', async (req, res) => {
     res.sendStatus(200);
 
     // Forward to Google Apps Script
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbyOQNCYrOpKsPJ4kgEBTqT6IrrpYTvNolswecOeeIggb5G0kbwhQVO8U5s-2IRZ2GPp/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbwJzEhjr-k0-rOkHdXHNUuWehU4XrU4YoT2GwicvIsEtAyhKP1AuALPSG3AAkH9q4AB/exec';
     const response = await axios.post(scriptUrl, { rowIndex });
 
     console.log('GAS response:', response.data);
